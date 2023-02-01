41f0df8f9ca48dcb0111b3e6baef568471ebb24e
Ricardo Luiz Ferreira <ferreirarl@mdck.local>
2023-02-01 16:40:34 -0300

    Exercicio 2 resolvido

--- questao02.js
+++ questao02.js
@@ -1,22 +1,21 @@
-let input1 = document.querySelector("#input1");
-let input2 = document.querySelector("#input2");
 let btnSoma = document.querySelector("#somar");
 let btnSubtrai = document.querySelector("#subtrair");
+let resultado = document.querySelector("#resultado");
 
 
 function capturaValores(){
-    let n1 = parseInt(input1.getAttribute('value'));
-    let n2 = parseInt(input2.getAttribute('value'));
+    let input1 = document.getElementById("input1").value;
+    let input2 = document.getElementById("input2").value;
 }
-input2.addEventListener("change",capturaValores());
 
-function somar(){
+btnSoma.onclick = function somar(){
     capturaValores();
-    let soma = n1 + n2
-    console.log (soma);
-}
-function substrair(){
+    let soma = parseInt(input1.value) + parseInt(input2.value)
+    resultado.innerHTML += (`Resultado:${soma}<br>`)
 }
 
-
-btnSoma.addEventListener("click",somar);
+btnSubtrai.onclick = function substrair(){
+    capturaValores();
+    let substracao = parseInt(input1.value) - parseInt(input2.value)
+    resultado.innerHTML += (`Resultado:${substracao}<br>`)
+}
