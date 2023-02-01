9ac6adf2a85c4c268f19136baf8946dd4ed807b0
Ricardo Luiz Ferreira <ferreirarl@mdck.local>
2023-02-01 16:42:03 -0300

    Identado corretamente

--- questao02.js
+++ questao02.js
@@ -2,20 +2,19 @@ let btnSoma = document.querySelector("#somar");
 let btnSubtrai = document.querySelector("#subtrair");
 let resultado = document.querySelector("#resultado");
 
-
-function capturaValores(){
-    let input1 = document.getElementById("input1").value;
-    let input2 = document.getElementById("input2").value;
+function capturaValores() {
+  let input1 = document.getElementById("input1").value;
+  let input2 = document.getElementById("input2").value;
 }
 
-btnSoma.onclick = function somar(){
-    capturaValores();
-    let soma = parseInt(input1.value) + parseInt(input2.value)
-    resultado.innerHTML += (`Resultado:${soma}<br>`)
-}
+btnSoma.onclick = function somar() {
+  capturaValores();
+  let soma = parseInt(input1.value) + parseInt(input2.value);
+  resultado.innerHTML += `Resultado:${soma}<br>`;
+};
 
-btnSubtrai.onclick = function substrair(){
-    capturaValores();
-    let substracao = parseInt(input1.value) - parseInt(input2.value)
-    resultado.innerHTML += (`Resultado:${substracao}<br>`)
-}
+btnSubtrai.onclick = function substrair() {
+  capturaValores();
+  let substracao = parseInt(input1.value) - parseInt(input2.value);
+  resultado.innerHTML += `Resultado:${substracao}<br>`;
+};
